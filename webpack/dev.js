c71eaffeec2e8f303dda65a18973baa9eacf8648
Umayr Shahid <umayrr@hotmail.co.uk>
2016-11-21 13:13:09 +0500

    Streamline babel/webpack configurations

--- webpack/dev.js
+++ webpack/dev.js
@@ -26,7 +26,7 @@ let config = Object.assign({}, baseConfig, {
 // Add needed loaders to the defaults here
 config.module.loaders.push({
   test: /\.(js|jsx)$/,
-  loader: 'react-hot!babel-loader?presets[]=es2015',
+  loader: 'babel-loader',
   include: [].concat(
     config.additionalPaths,
     [path.join(__dirname, '/../src')]
